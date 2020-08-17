c81586652a4d20281653e864fdb369037986efd7
Hallef Santos <hallefsantos@gmail.com>
2020-08-17 10:16:06 -0300

    Add toggle menu

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,30 @@
 <script>
-	
+	import Header from './components/Header.svelte'
 </script>
-
-<main>
-
-</main>
 
 <style>
 
 </style>
+
+<div class="bg-gray-100">
+	<Header />
+
+	<main class="mt-10">
+		<div class="px-4">
+			<h1 class="mx-5 text-center text-gray-800 text-2xl font-bold leading-snug">
+				<span class="inline-flex relative">
+					<span class="absolute inset-0 bg-indigo-600" style="transform: skewY(-.5deg) scaleX(1.05)"></span>
+					<span class="text-white relative">Pays for itself</span>
+				</span>
+				<span class="ml-1">in happier, more productive engineers</span> 
+			</h1>
+
+			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam suscipit minima, voluptatibus pariatur impedit harum possimus dolores, similique laboriosam quisquam ratione provident qui voluptas error quos, explicabo velit fuga reprehenderit.</p>
+			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam suscipit minima, voluptatibus pariatur impedit harum possimus dolores, similique laboriosam quisquam ratione provident qui voluptas error quos, explicabo velit fuga reprehenderit.</p>
+			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam suscipit minima, voluptatibus pariatur impedit harum possimus dolores, similique laboriosam quisquam ratione provident qui voluptas error quos, explicabo velit fuga reprehenderit.</p>
+			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam suscipit minima, voluptatibus pariatur impedit harum possimus dolores, similique laboriosam quisquam ratione provident qui voluptas error quos, explicabo velit fuga reprehenderit.</p>
+			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam suscipit minima, voluptatibus pariatur impedit harum possimus dolores, similique laboriosam quisquam ratione provident qui voluptas error quos, explicabo velit fuga reprehenderit.</p>
+			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam suscipit minima, voluptatibus pariatur impedit harum possimus dolores, similique laboriosam quisquam ratione provident qui voluptas error quos, explicabo velit fuga reprehenderit.</p>
+		</div>
+	</main>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: [],
   theme: {
